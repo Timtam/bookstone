7130a60d7a01bb5b347800bb52d639d2c550ca5e
Toni Barth <software@satoprogs.de>
2020-01-19 11:54:57 +0100

    add question before closing if indexing is currently in progress

--- src/main/python/ui/window.py
+++ src/main/python/ui/window.py
@@ -1,6 +1,32 @@
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import (
+  QWidget,
+  QMessageBox)
+
+from storage import Storage
 
 class Window(QWidget):
 
   closed = pyqtSignal()
+
+  def closeEvent(self, event):
+  
+    manager = Storage.getInstance().getLibraryManager()
+    
+    if not manager.isIndexing():
+      event.accept()
+      return
+    
+    box = QMessageBox()
+    box.setText("Indexing operation in progress")
+    box.setInformativeText("Do you really want to exit and abort the operation?")
+    box.setStandardButtons( QMessageBox.Yes | QMessageBox.No )
+    box.setDefaultButton( QMessageBox.No )
+    box.setIcon( QMessageBox.Question )
+
+    res = box.exec_()
+
+    if res == QMessageBox.Yes:
+      event.accept()
+    else:
+      event.ignore()
