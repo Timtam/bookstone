b79c7f841a0b229c84ff34301f46e6a9591b2811
Toni Barth <software@satoprogs.de>
2020-10-22 15:02:45 +0200

    started adding detection patterns to tags using the external regex library

--- src/main/python/library/tag.py
+++ src/main/python/library/tag.py
@@ -1,4 +1,7 @@
 import copy
+from typing import Any, cast
+
+import regex
 
 
 class Tag:
@@ -6,12 +9,14 @@ class Tag:
     value: str
     _name: str
     _default: str
+    _pattern: regex.Regex
 
-    def __init__(self, name: str, value: str = "") -> None:
+    def __init__(self, name: str, value: str = "", pattern: str = "") -> None:
 
         self._name = name
         self.value = value
         self._default = value
+        self._pattern = regex.compile(f"(?P<{self._name}>{pattern})")
 
     def isModified(self) -> bool:
         return self.value != self._default
@@ -30,6 +35,10 @@ class Tag:
     def default(self) -> str:
         return self._default
 
+    @property
+    def pattern(self) -> regex.Regex:
+        return self._pattern
+
     def __str__(self) -> str:
         return "Tag[{name}] = {value}".format(name=self._name, value=self.value)
 
@@ -39,3 +48,15 @@ class Tag:
     def __call__(self) -> "Tag":
 
         return copy.copy(self)
+
+    def __eq__(self, other: Any) -> bool:
+
+        if isinstance(other, Tag):
+            return (
+                self._name == cast(Tag, other)._name
+                and self.value == cast(Tag, other).value
+            )
+        elif isinstance(other, str):
+            return self._name == other
+
+        return NotImplemented
--- src/main/python/library/tags.py
+++ src/main/python/library/tags.py
@@ -3,9 +3,8 @@ from typing import Tuple
 from .tag import Tag
 
 Tags: Tuple[Tag, ...] = (
-    Tag("author", "Unknown Author"),
-    Tag("title", "Unknown Title"),
-    Tag("series"),
-    Tag("collection"),
-    Tag("number"),
+    Tag("author", "Unknown Author", r"(?:[^\W\d_]| )+"),
+    Tag("title", "Unknown Title", r"\w++(?:[.,_:()\s-](?![.\s-])|\w++)*"),
+    Tag("series", pattern=r"\w++(?:[.,_:()\s-](?![.\s-])|\w++)*"),
+    Tag("number", pattern=r"\d+"),
 )
