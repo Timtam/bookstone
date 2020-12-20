84c0067e70d8261ca91d7c5f6263eaf1e7bf48a4
Toni Barth <software@satoprogs.de>
2020-12-20 12:28:31 +0100

    fix windows closing the entire application instead of only the current window

--- src/main/python/ui/window.py
+++ src/main/python/ui/window.py
@@ -5,3 +5,12 @@ from PyQt5.QtWidgets import QWidget
 class Window(QWidget):
 
     closed: pyqtSignal = pyqtSignal()
+
+    def close(self) -> bool:
+
+        success: bool = super().close()
+
+        if success:
+            self.closed.emit()
+
+        return success
