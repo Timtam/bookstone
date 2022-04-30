9ad45d6dcb790ec1cd6bf0156e93be3e061981f9
Toni Barth <software@satoprogs.de>
2022-04-30 18:51:51 +0200

    add optional profiling to launcher add splash screen to launcher when waiting for libraries to load

--- bookstone/__main__.py
+++ bookstone/__main__.py
@@ -1,13 +1,25 @@
-import sys
+import cProfile
+import pstats
+
+from PyQt5.QtWidgets import QSplashScreen
 
 import utils
 from container import Container
 
-if __name__ == "__main__":
+
+def main() -> None:
 
     container = Container()
 
     app = container.application()
+
+    splash_screen = QSplashScreen()
+
+    splash_screen.show()
+
+    splash_screen.showMessage("Loading configuration...")
+
+    app.processEvents()  # type: ignore
 
     conf_manager = container.configuration_manager()
     conf_manager.load(utils.getConfigFile())
@@ -15,13 +27,39 @@ if __name__ == "__main__":
     lib_manager = container.library_manager()
 
     am = container.audio_manager()
+
+    splash_screen.showMessage("Loading libraries...")
+
+    app.processEvents()  # type: ignore
+
     lib_manager.load()
+
+    splash_screen.showMessage("Initializing audio...")
+
+    app.processEvents()  # type: ignore
+
     am.initialize()
 
-    controller = container.ui.window_controller()
-    controller.pushWindow(container.ui.main_window())
+    window = container.ui.main_window()
 
-    exit_code: int = app.exec_()
+    controller = container.ui.window_controller()
+    controller.pushWindow(window)
+    splash_screen.finish(window)
+
+    app.exec_()
     am.uninitialize()
     conf_manager.save(utils.getConfigFile())
-    sys.exit(exit_code)
+
+
+def run_profile(code: str) -> None:
+
+    cProfile.runctx(code, globals(), locals(), "Profile.prof")
+
+    s = pstats.Stats("Profile.prof")
+    s.strip_dirs().sort_stats("time").print_stats()
+
+
+if __name__ == "__main__":
+
+    main()
+    # run_profile("main()")
